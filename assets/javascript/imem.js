5a8509ea04af4c9521350063dd077a64cb405e87
Ian-Maynard <maynard.ian@gmail.com>
2017-09-01 19:29:58 -0400

    slide_one

---
+++ assets/javascript/imem.js
@@ -0,0 +1,20 @@
+ $('.button-collapse').sideNav({
+      menuWidth: 400, // Default is 300
+      // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) {  },
+      onClose: function(el) { }, // A function to be called when sideNav is closed
+    });
+   $('collapsible').collapsible();
+
+ $('.button-collapse1').sideNav({
+      edge: 'right', // Choose the horizontal origin
+      menuWidth: 400, // Default is 300
+      // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) {  },
+      onClose: function(el) { }, // A function to be called when sideNav is closed
+    });
+   $('collapsible').collapsible();
